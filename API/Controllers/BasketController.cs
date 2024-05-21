0c083aa2fb73d3621097ee90ce24303e1696d5fb
MikaZ21 <zukeyamamika@gmail.com>
2024-05-21 17:55:36 -0500

    Section 16: 182-183. Validating the basket / Checking for duplicate email addresses. API - Validation

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,3 +1,5 @@
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
@@ -7,9 +9,12 @@ namespace API.Controllers
 {
     public class BasketController : BaseApiController
     {
+        private readonly IMapper _mapper;
         private readonly IBasketRepository _basketRepository;
-        public BasketController(IBasketRepository basketRepository)
+
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _basketRepository = basketRepository;
         }
 
@@ -22,9 +27,10 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
         {
-            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            var CustomerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(CustomerBasket);
 
             return Ok(updatedBasket);
         }
