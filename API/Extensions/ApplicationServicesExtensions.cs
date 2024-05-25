8665c0e11ea7364270d5f198a20a2954921d797c
MikaZ21 <zukeyamamika@gmail.com>
2024-05-24 19:07:11 -0500

    Section 16: 184. Updating swagger config for identity. API - Validation

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -4,7 +4,6 @@ using Infrastructure.Data;
 using Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using StackExchange.Redis;
 
 namespace API.Extensions
@@ -14,9 +13,6 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services,
             IConfiguration config)
             {
-                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-                services.AddEndpointsApiExplorer();
-                services.AddSwaggerGen();
                 services.AddDbContext<StoreContext>(x =>
                 {
                     x.UseSqlite(config.GetConnectionString("DefaultConnection"));
