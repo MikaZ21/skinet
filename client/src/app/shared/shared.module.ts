c52465dea588970361f2ee00fbed70edebb06892
MikaZ21 <zukeyamamika@gmail.com>
2024-05-29 11:01:35 -0500

    Section 17: 195. Adding a dropdown to the navbar. Client - Identity

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -6,7 +6,7 @@ import { PagerComponent } from './pager/pager.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './order-totals/order-totals.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
 @NgModule({
@@ -20,6 +20,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     PaginationModule.forRoot(),
     CarouselModule.forRoot(),
     ReactiveFormsModule,
+    BsDropdownModule.forRoot()
   ],
   exports: [
     PaginationModule,
@@ -28,6 +29,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     CarouselModule,
     OrderTotalsComponent,
     ReactiveFormsModule,
+    BsDropdownModule
   ]
 })
 export class SharedModule { }
