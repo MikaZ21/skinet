33cb3d5cd5d763cbac58fa301d0f838d78c564f0
MikaZ21 <zukeyamamika@gmail.com>
2024-07-10 18:19:41 -0500

    Section18: 224-225. Testing the order controller get methods, Shaping the order data API - Orders

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -36,17 +36,17 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Order>>> GetOrderForUser()
+        public async Task<ActionResult<IReadOnlyList<OrderToReturnDto>>> GetOrderForUser()
         {
             var email = HttpContext.User.RetrieveEmailFromPrincipal();
 
             var orders = await _orderService.GetOrderForUserAsync(email);
 
-            return Ok(orders);
+            return Ok(_mapper.Map<IReadOnlyList<OrderToReturnDto>>(orders));
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Order>> GetOrderByIdForUser(int id)
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderByIdForUser(int id)
         {
             var email = HttpContext.User.RetrieveEmailFromPrincipal();
 
@@ -54,7 +54,7 @@ namespace API.Controllers
 
             if (order == null) return NotFound(new ApiResponse(404));
 
-            return order;
+            return _mapper.Map<OrderToReturnDto>(order);
         }
 
         [HttpGet("deliveryMethods")]
