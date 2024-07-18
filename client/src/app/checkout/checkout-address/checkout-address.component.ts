04a435635e186729d9c8c751216efb912b2482a9
MikaZ21 <zukeyamamika@gmail.com>
2024-07-18 17:18:41 -0500

    Section 19: 244. Letting the user save the address Client - Checkout

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/account/account.service';
+import { addEmitHelper } from 'typescript';
 
 @Component({
   selector: 'app-checkout-address',
@@ -8,4 +11,15 @@ import { FormGroup } from '@angular/forms';
 })
 export class CheckoutAddressComponent {
   @Input() checkoutForm?: FormGroup;
+
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+
+  saveUserAddress() {
+    this.accountService.updateUserAddress(this.checkoutForm?.get('addressForm')?.value).subscribe({
+      next: () => {
+        this.toastr.success('Address saved');
+        this.checkoutForm?.get('addressForm')?.reset(this.checkoutForm?.get('addressForm')?.value);
+      }
+    })
+  }
 }
