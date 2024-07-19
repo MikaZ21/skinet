b91366351984c69e3769de9e25ab33a230c36b64
MikaZ21 <zukeyamamika@gmail.com>
2024-07-19 16:49:41 -0500

    Section 19: 247. Order submission Client - Checkout

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { BasketService } from 'src/app/basket/basket.service';
+import { CheckoutService } from '../checkout.service';
+import { ToastrService } from 'ngx-toastr';
+import { Address } from 'src/app/shared/models/user';
+import { Basket } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-checkout-payment',
@@ -6,5 +12,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkout-payment.component.scss']
 })
 export class CheckoutPaymentComponent {
+  @Input() checkoutForm?: FormGroup;
 
+  constructor(private basketService: BasketService, private checkoutService: CheckoutService,
+    private toastr: ToastrService) {}
+  
+  submitOrder() {
+    const basket = this.basketService.getCurrentBasketValue();
+    if (!basket) return;
+    const OrderToCreate = this.getOrderToCreate(basket);
+    if (!OrderToCreate) return;
+    this.checkoutService.createOrder(OrderToCreate).subscribe({
+      next: order => {
+        this.toastr.success('Order created successfully');
+        console.log(order);
+      }
+    })
+  }
+
+  private getOrderToCreate(basket: Basket) {
+    const deliveryMethodId = this.checkoutForm?.get('deliveryForm')?.get('deliveryMethod')?.value;
+    const shipToAddress = this.checkoutForm?.get('addressForm')?.value as Address;
+    if (!deliveryMethodId || !shipToAddress) return;
+    return {
+      basketId: basket.id,
+      deliveryMethodId: deliveryMethodId,
+      shipToAddress: shipToAddress
+    }
+  }
 }
