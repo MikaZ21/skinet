952b7c165704d4b0ed4a55e91a2c1bc44d14d234
MikaZ21 <zukeyamamika@gmail.com>
2024-07-26 15:52:57 -0500

    Section 20-3: Lazy load the orders module in the app-routing.module.ts - Orders

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -19,8 +19,17 @@ const routes: Routes = [
     loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule),
 
   },
-  {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
-  {path: '**', redirectTo: '', pathMatch: 'full'},
+  {
+    path: 'orders',
+    canActivate:[AuthGuard],
+    loadChildren: () => import('./orders/orders.module').then(mod => mod.OrdersModule),
+    data: { breadcrumb: 'Orders' }
+  },
+  {
+    path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule),
+    data: {breadcrumb: { skip: true }}
+  },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full'},
 ];
 
 @NgModule({
