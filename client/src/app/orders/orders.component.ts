8802369174d1423aa35c6c6ea931287637210ef8
MikaZ21 <zukeyamamika@gmail.com>
2024-07-27 10:58:22 -0500

    Section 20-5: Created the orders list in the orders.component.ts and it's template - Orders

--- client/src/app/orders/orders.component.ts
+++ client/src/app/orders/orders.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Order } from '../shared/models/order';
+import { OrdersService } from './orders.service';
 
 @Component({
   selector: 'app-orders',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent {
+  orders: Order[] = [];
 
+  constructor(private orderService: OrdersService) {}
+
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.orderService.getOrdersForUser().subscribe({
+      next: orders => this.orders = orders
+    })
+  }
 }
