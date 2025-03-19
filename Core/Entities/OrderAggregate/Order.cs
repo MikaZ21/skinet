db20652cc8e7bdbb60a92185091e9c56f6bd5ccd
MikaZ21 <zukeyamamika@gmail.com>
2025-03-19 13:03:05 -0500

    21-17: Trying the payment intent to the order

--- Core/Entities/OrderAggregate/Order.cs
+++ Core/Entities/OrderAggregate/Order.cs
@@ -5,15 +5,17 @@ namespace Core.Entities.OrderAggregate
         public Order()
         {
         }
+        private readonly string _PaymentIntentId;
 
         public Order(IReadOnlyList<OrderItem> orderItems, string buyerEmail, 
-                    Address shipToAddress, DeliveryMethod deliveryMethod, decimal subtotal)
+                    Address shipToAddress, DeliveryMethod deliveryMethod, decimal subtotal, string paymentIntentId)
         {
             BuyerEmail = buyerEmail;
             ShipToAddress = shipToAddress;
             DeliveryMethod = deliveryMethod;
             OrderItems = orderItems;
             Subtotal = subtotal;
+            PaymentIntentId = paymentIntentId;
         }
 
         public string BuyerEmail { get; set; }
